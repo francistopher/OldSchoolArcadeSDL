dc0b543916089b62327c46231ea83e8b8af16cef
francistopher <francistopherrr@outlook.com>
2022-06-18 01:08:02 -0400

    UPDATE main.c to draw shitty ball

--- pong/main.c
+++ pong/main.c
@@ -9,6 +9,15 @@
 
 #define WIDTH 640
 #define HEIGHT 640
+#define BALL_LENGTH 16
+
+struct RGBA
+{
+	int r, g, b, a;
+};
+
+void drawShittyBall(SDL_Renderer *renderer, SDL_Rect rect, struct RGBA color);
+
 
 int main(int argc, const char* argv[])
 {
@@ -20,11 +29,21 @@ int main(int argc, const char* argv[])
 	SDL_Window *window = SDL_CreateWindow("Pong 1.0", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN); // create window
 
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); // create renderer
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // draw background
 	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
 
-	SDL_Event event;
+	struct RGBA white = {.r = 255, .g = 255, .b = 255, .a = 255}; // draw shitty pixelated ball
+	SDL_Rect ballRect = {
+		.x = (WIDTH - BALL_LENGTH) / 2,
+		.y = (HEIGHT - BALL_LENGTH) / 2,
+		.w = BALL_LENGTH,
+		.h = BALL_LENGTH,
+	};
+	drawShittyBall(renderer, ballRect, white);
+
+	SDL_RenderPresent(renderer); // have renderer present
+
+	SDL_Event event;  // create event
 
 	while(SDL_WaitEvent(&event)) { // start events loop
 		if (event.type == SDL_QUIT) {
@@ -34,4 +53,9 @@ int main(int argc, const char* argv[])
 
 
 	SDL_Quit(); // quit
+}
+
+void drawShittyBall(SDL_Renderer *renderer, SDL_Rect rect, struct RGBA color) {
+	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+	SDL_RenderFillRect(renderer, &rect);
 }
